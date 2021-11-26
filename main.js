f2b0ee4dd45a69b73cd8444bd49bafda9848c52a
Mai Tri Toan <69956369+likeoneringfake@users.noreply.github.com>
2021-11-26 12:36:25 +0700

    xong 1 khusc

--- main.js
+++ main.js
@@ -8,3 +8,47 @@ window.onscroll = function () {
 		header.classList.remove('active')
 	}
 }
+const dataInfo = [
+	{
+		img: './assect/images/Icon-Sythetic.png',
+		title: 'SYNTHETIC CURVE',
+		description:
+			'Normal oracles update only the price data on-chain. The synthetic curve feature constantly synchronizes BOTH the price AND DEX liquidity on chain',
+	},
+	{
+		img: './assect/images/Icon-Slippage.png',
+		title: 'ANTI SLIPPAGE',
+		description:
+			'Expected price on a trade and actual price executed become closer. Which means you enter and exit small cap tokens with ease at a lower price.',
+	},
+	{
+		img: './assect/images/Icon-Sythetic.png',
+		title: 'SYNTHETIC CURVE',
+		description:
+			'Normal oracles update only the price data on-chain. The synthetic curve feature constantly synchronizes BOTH the price AND DEX liquidity on chain',
+	},
+	{
+		img: './assect/images/Icon-Sythetic.png',
+		title: 'SYNTHETIC CURVE',
+		description:
+			'Normal oracles update only the price data on-chain. The synthetic curve feature constantly synchronizes BOTH the price AND DEX liquidity on chain',
+	},
+	{
+		img: './assect/images/Icon-Sythetic.png',
+		title: 'SYNTHETIC CURVE',
+		description:
+			'Normal oracles update only the price data on-chain. The synthetic curve feature constantly synchronizes BOTH the price AND DEX liquidity on chain',
+	},
+	{
+		img: './assect/images/Icon-Sythetic.png',
+		title: 'SYNTHETIC CURVE',
+		description:
+			'Normal oracles update only the price data on-chain. The synthetic curve feature constantly synchronizes BOTH the price AND DEX liquidity on chain',
+	},
+	{
+		img: './assect/images/Icon-Sythetic.png',
+		title: 'SYNTHETIC CURVE',
+		description:
+			'Normal oracles update only the price data on-chain. The synthetic curve feature constantly synchronizes BOTH the price AND DEX liquidity on chain',
+	},
+]
